5f0ef951aaad421dd679d8938cf5151b3c14b542
Gustavo Damasceno <gustavoxaviercontato@gmail.com>
2024-03-19 21:03:54 -0300

    update png

--- src/main/java/com/lesmonades/messaging/repositories/AccountRepository.java
+++ src/main/java/com/lesmonades/messaging/repositories/AccountRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository extends MongoRepository<Account, Integer> {
+public interface AccountRepository extends MongoRepository<Account, String> {
 
     Account findByNumber(int accountNumber);
 }
